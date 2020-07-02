e15839a4bebb68608b44b93d331f72a82acecf00
grogsden <brian.stempien@gmail.com>
2020-07-02 10:34:35 -0500

    Express part 2

--- server.js
+++ server.js
@@ -1,38 +1,4 @@
-const express = require('express');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-
-const hostname = 'localhost';
-const port = 3000;
-
-const app = express();
-app.use(morgan('dev'));
-app.use(bodyParser.json());
-
-app.all('/campsites', (req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-});
-
-app.get('/campsites', (req, res) => {
-    res.end('Will send all the campsites to you');
-});
-
-app.post('/campsites', (req, res) => {
-    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
-});
-
-app.put('/campsites', (req, res) => {
-    res.statusCode = 403;
-    res.end('PUT operation not supported on /campsites');
-});
-
-app.delete('/campsites', (req, res) => {
-    res.end('Deleting all campsites');
-});
-
-app.get('/campsites/:campsiteId', (req, res) => {
+/* app.get('/campsites/:campsiteId', (req, res) => {
     res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
 });
 
@@ -49,7 +15,21 @@ app.put('/campsites/:campsiteId', (req, res) => {
 
 app.delete('/campsites/:campsiteId', (req, res) => {
     res.end(`Deleting campsite: ${req.params.campsiteId}`);
-});
+}); */
+
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const campsiteRouter = require('./routes/campsiteRouter');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const app = express();
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.use('/campsites', campsiteRouter);
 
 app.use(express.static(__dirname + '/public'));
 
